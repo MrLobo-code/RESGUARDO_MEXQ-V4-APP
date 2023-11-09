1357d2171cff086b23e6e199b6e28e671839ea11
unknown <oscarortiz016ags@gmail.com>
2023-11-09 17:59:17 -0600

    borré algunos comentarios y lineas innecesarias

--- src/app/components/equiposDeComputo/mybody/equiposBody-component.component.ts
+++ src/app/components/equiposDeComputo/mybody/equiposBody-component.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EquiposTableService } from '../../../service/equipos-table.service';
-import { AxiosResponse } from 'axios';
+// import { AxiosResponse } from 'axios';
 
-// interface equipo {          these helps to 
+// interface equipo {          these helps to
 // id: number,
 // codigo: number,
 // }
@@ -22,23 +22,3 @@ export class equiposBodyComponent implements OnInit{
     this.equipos = await this.equiposService.getEquipos();
   }
 }
-
-// import { Component, OnInit } from '@angular/core';
-// import { EquiposTableService } from '../../../service/equipos-table.service';
-// import { AxiosResponse } from 'axios';
-
-// @Component ({
-//     selector: 'equiposBody',
-//     templateUrl: './equiposBody-component.component.html',
-//     styleUrls: ['./equiposBody-component.component.css']
-// })
-// export class equiposBodyComponent implements OnInit{
-
-//   equipos: Promise<AxiosResponse<any, any>> = Promise.resolve([]);
-
-//   constructor(private equiposService: EquiposTableService) {}
-  
-//   ngOnInit(): void {
-//     this.equipos = this.equiposService.getEquipos();
-//   }
-// }
